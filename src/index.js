0155df00771c2a5ebc63625c5b5ec72b16e8d8c1
Yuvant Besre <yuvisingh121212@gmail.com>
2022-01-30 18:56:26 +0530

    Add base url

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import './style.css'
 import '@riotjs/hot-reload'
 import { mount } from 'riot'
 import registerGlobalComponents from './register-global-components'
+import axios from "axios"
+
+axios.defaults.baseURL = "http://localhost:8000"
 
 // register
 registerGlobalComponents()
